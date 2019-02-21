1414389f28c6af6c931af9155707fd0d4e18c87b
Vikas Viki <vikasviki489@gmail.com>
2019-02-21 14:48:35 +0530

    FEB19 -- Cook Off -- Querstion 2
    Question link : https://www.codechef.com/COOK103B/problems/CHFPARTY/
    Solution link : https://www.codechef.com/viewsolution/23129294

---
+++ Codechef/Cook-Off/FEB19/CHFPARTY.py
@@ -0,0 +1,16 @@
+def main():
+    testcases = int(input())
+    for test in range(testcases):
+        no_of_friends = int(input())
+        friends = list(map(int,input().split()))
+        people_at_party = 0
+        friends.sort()
+        for friend in friends:
+            if people_at_party >= friend:
+                people_at_party += 1
+            else:
+                break
+        print(people_at_party)
+
+if __name__ == "__main__":
+    main()
