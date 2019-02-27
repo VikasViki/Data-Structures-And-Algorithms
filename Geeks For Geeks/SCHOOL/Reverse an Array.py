2af674581e2d9c396321de5bb7603514c450c162
Vikas Viki <vikasviki489@gmail.com>
2019-02-28 00:11:28 +0530

    School -- Question -- 1

---
+++ Geeks For Geeks/SCHOOL/Reverse an Array.py
@@ -0,0 +1,9 @@
+## Question Link : https://practice.geeksforgeeks.org/problems/reverse-an-array/0
+
+n = int(input())
+l = list(map(int,input().split()))
+for i in range(n-1,-1,-1):
+    print(l[i],end=" ")
+print()
+
+##Solution Link : https://practice.geeksforgeeks.org/viewSol.php?subId=10681879&pid=78&user=VikasViki
