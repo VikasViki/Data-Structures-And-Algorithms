56716abde010c91ec6a54d00ba7727f83b98a667
Vikas Viki <vikasviki489@gmail.com>
2019-03-01 21:49:27 +0530

    School -- Question -- 2

---
+++ Geeks For Geeks/SCHOOL/Print alternate elements of an array.py
@@ -0,0 +1,16 @@
+## Question Link : https://practice.geeksforgeeks.org/problems/print-alternate-elements-of-an-array/1
+
+
+''' Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above. '''
+
+#User function Template for python3
+# arr is the array
+# n is the number of elements in array
+def printAl(arr,n):
+    # your code here
+    for index in range(0,n,2):
+        print(arr[index],end=" ")
+        
+## Solution Link : https://practice.geeksforgeeks.org/viewSol.php?subId=12549915&pid=700712&user=VikasViki
