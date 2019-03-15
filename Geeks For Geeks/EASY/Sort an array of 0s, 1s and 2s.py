ae02c812417497cd9e9e8375e941e9d59ae212a9
Vikas Viki <vikasviki489@gmail.com>
2019-03-15 23:40:58 +0530

    Question -- 1

---
+++ Geeks For Geeks/EASY/Sort an array of 0s, 1s and 2s.py
@@ -0,0 +1,6 @@
+##Question link : https://practice.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s/0
+    n = int(input())
+    l = list(map(int,input().split()))
+    l.sort()
+    print(*l)
+##Solution link : https://practice.geeksforgeeks.org/viewSol.php?subId=9594302&pid=341&user=VikasViki
