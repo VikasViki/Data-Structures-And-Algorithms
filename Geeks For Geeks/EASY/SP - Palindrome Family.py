99f0b41f1bb4910dedcb16060198251783a0de47
Vikas Viki <vikasviki489@gmail.com>
2019-03-20 01:53:53 +0530

    Question -- 5

---
+++ Geeks For Geeks/EASY/SP - Palindrome Family.py
@@ -0,0 +1,26 @@
+## Question link : https://practice.geeksforgeeks.org/problems/sp-palindrome-family/0
+
+    if string[:] == string[::-1]:
+        return True
+    else:
+        return False
+        
+for _ in range(int(input())):
+    s = input()
+    if pal(s):
+        print("PARENT")
+    else:
+        even = "".join([s[i] for i in range(len(s)) if i%2 != 0])
+        odd = "".join([s[i] for i in range(len(s)) if i%2 == 0])
+        pal_even = pal(even)
+        pal_odd = pal(odd)
+        if pal_even and pal_odd:
+            print("TWIN")
+        elif pal_even:
+            print("EVEN")
+        elif pal_odd:
+            print("ODD")
+        else:
+            print("ALIEN")
+
+## Solution link : https://practice.geeksforgeeks.org/viewSol.php?subId=7357149&pid=3507&user=VikasViki
