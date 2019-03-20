af38c38f6f2ab1e3bf99ef21145d2221100fe5e1
Vikas Viki <vikasviki489@gmail.com>
2019-03-21 00:43:25 +0530

    Question -- 6

---
+++ Geeks For Geeks/EASY/Sum of given range.py
@@ -0,0 +1,13 @@
+## Question link : https://practice.geeksforgeeks.org/problems/sum-of-given-range/1
+
+
+''' Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above. '''
+
+#User function Template for python3
+def getSum(arr,low,high):
+    #Add code here
+    print(sum(arr[low:high+1]))
+    
+## Solution link : https://practice.geeksforgeeks.org/viewSol.php?subId=12926043&pid=700531&user=VikasViki
