95d8feca3c9c107b0244b2bebaa953bc10e1d537
Vikas Viki <vikasviki489@gmail.com>
2019-03-24 01:55:58 +0530

    Question -- 14

---
+++ Geeks For Geeks/BASIC/Start Coding - Python.py
@@ -0,0 +1,14 @@
+## Question link : https://practice.geeksforgeeks.org/problems/start-coding-python/1
+
+''' Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above. '''
+
+#User function Template for python3
+def print_fun():
+    
+    # Your code here
+    # Please follow proper indentation
+    print("Hello World")
+    
+## Solution link : https://practice.geeksforgeeks.org/viewSol.php?subId=12320112&pid=700930&user=VikasViki
