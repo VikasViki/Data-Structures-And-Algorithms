156877836252719bcaa9b688cd02d141c6fcd118
Vikas Viki <vikasviki489@gmail.com>
2019-03-25 00:48:39 +0530

    Question -- 15

---
+++ Geeks For Geeks/BASIC/Space Seperated - Python.py
@@ -0,0 +1,16 @@
+## Question link : https://practice.geeksforgeeks.org/problems/space-seperated-python/1
+
+
+''' Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above. '''
+
+#User function Template for python3
+# Function to print words with spaces
+def print_fun():
+    # Your code here
+    # Do not use space in the string
+    # try to use seperator
+    print("Geeks","For","Geeks")
+    
+## Solution link : https://practice.geeksforgeeks.org/viewSol.php?subId=12320158&pid=700932&user=VikasViki
