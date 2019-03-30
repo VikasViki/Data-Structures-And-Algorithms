c80781032ce8c9dc4d3ef80dc6db6df3f8860616
Vikas Viki <vikasviki489@gmail.com>
2019-03-31 00:08:51 +0530

    Question -- 4

---
+++ Geeks For Geeks/MEDIUM/Multiply two strings.py
@@ -0,0 +1,13 @@
+## Question link : https://practice.geeksforgeeks.org/problems/multiply-two-strings/1
+
+
+''' Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above. '''
+
+#User function Template for python3
+def multiply(a,b):
+    #add code here
+    print(int(a)*int(b))
+
+## Solution link : https://practice.geeksforgeeks.org/viewSol.php?subId=10587980&pid=700383&user=VikasViki
